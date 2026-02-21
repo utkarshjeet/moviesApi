41812be7a4253a4d6cf4d7af9a1278f320b045d3
utkarshjeet <utkarshyadav1029@gmail.com>
2026-02-21 11:15:25 +0530

    starting

---
+++ script.js
@@ -0,0 +1,61 @@
+const APILINK = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+const main = document.getElementById('section');
+const form = document.getElementById('form');
+const search = document.getElementById('query');
+
+function returnMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results);
+        data.results.forEach(element => {
+            const div_card = document.createElement('div');
+            const div_column = document.createElement('div');
+            const div_row = document.createElement('div');
+            const img = document.createElement('img');
+            const title = document.createElement('h3');
+            const center = document.createElement('center');
+
+            div_card.setAttribute('class', 'card');
+            div_column.setAttribute('class', 'column');
+            div_row.setAttribute('class', 'row');
+            img.setAttribute('class', 'thumbnail');
+            img.setAttribute('src', IMG_PATH + element.poster_path);
+            title.appendChild(document.createTextNode(element.title));
+            center.appendChild(img);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+            section.appendChild(div_row);
+
+            title.innerHTML = `${element.title}`;
+            img.src = `${IMG_PATH + element.poster_path}`;
+
+            center.appendChild(img);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+            section.appendChild(div_row);
+
+            main.appendChild(div_row);
+
+
+
+        });
+    });
+}
+
+returnMovies(APILINK);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    const searchTerm = search.value;
+    if (searchTerm) {
+        returnMovies(SEARCH_API + searchTerm);
+        search.value = '';
+    }
+});
